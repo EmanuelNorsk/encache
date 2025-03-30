89405c66b0bc06d054bca498b735d4f8792fdea1
EmanuelNorsk <emanuelodenhesselroth@gmail.com>
2025-03-30 23:06:32 +0200

    Updated setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="encache", 
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),  
     install_requires=[], 
     description="A small and easy caching system for every use",
